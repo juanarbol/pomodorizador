7e674a6c059eae23ae861e498a4c50e25cea01ac
Juan José Arboleda <soyjuanarbol@gmail.com>
2019-04-02 22:20:19 -0500

    Updates

---
+++ precache-manifest.45b3cb3a1fa3fb7cf2368d628329c60f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "667232dec62f61f0dd36",
+    "url": "/static/js/main.fe5dcf61.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "667232dec62f61f0dd36",
+    "url": "/static/css/main.62e9a472.chunk.css"
+  },
+  {
+    "revision": "2a4317b4d0c7ce1c7d9ad76f8c6b1457",
+    "url": "/index.html"
+  }
+];
